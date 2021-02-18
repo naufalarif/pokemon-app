0244aaa2419e8f88ff55b5ca5658fdb08b164028
naufal <naufalarif92@gmail.com>
2021-02-18 19:01:58 +0700

    test pwa

--- pages/_document.js
+++ pages/_document.js
@@ -10,8 +10,20 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta charset='utf-8' />
+          <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+          <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
+          <meta name='description' content='Pokemon App is a mini game to catch pokemon' />
+          <meta name='keywords' content='pokemon' />
+          <title>Pokemon App</title>
+
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;400;700;900&display=swap" rel="stylesheet"/>
+          <link rel='manifest' href='/manifest.json' />
+          <link href='/icon.png' rel='icon' type='image/png' sizes='16x16' />
+          <link href='/icon.png' rel='icon' type='image/png' sizes='32x32' />
+          <link rel='apple-touch-icon' href='/icon.png'></link>
+          <meta name='theme-color' content='#fff' />
           {/* <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700;800&display=swap" rel="stylesheet"/> */}
         </Head>
