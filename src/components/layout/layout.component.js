91b6b99e113141b326b0e5a11195b1e8187d45fb
Naufal Arif <naufalarif92@gmail.com>
2023-04-04 14:59:12 +0700

    chore(layout): update layout component

--- src/components/layout/layout.component.js
+++ src/components/layout/layout.component.js
@@ -1,12 +1,14 @@
+import isEmpty from 'lodash/isEmpty';
+
 import Footer from '../footer';
 import Navbar from '../navbar';
 import styles from './layout.module.css';
 
-export default function Layout({ children, active }) {
+export default function Layout({ children, active = '' }) {
   return (
     <div className={styles.layout}>
       <Navbar active={active}/>
-      {active !== "adventure" ? (
+      {active !== "adventure" || !isEmpty(active) ? (
         <div className="wrapper">
           {children}
         </div>
